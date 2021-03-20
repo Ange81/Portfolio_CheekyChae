9fa21a11b5f2422e168cbd16654cebbf1bce4784
Ange <ajhchae@gmail.com>
2021-03-20 10:58:10 +0000

    add auto scroll for process bar

--- js/script.js
+++ js/script.js
@@ -13,4 +13,16 @@ $(window).scroll(function() {
   } else {
     $('.index').fadeOut();
   }
-}) 
+})
+
+$('.scroll').on('click',function(e) {
+	e.preventDefault();
+	var offset = 0;
+	var target = this.hash;
+	if ($(this).data('offset') != undefined) offset = $(this).data('offset');
+	$('html, body').stop().animate({
+		'scrollTop': $(target).offset().top - offset
+	}, 500, 'swing', function() {
+		// window.location.hash = target;
+	});
+});
